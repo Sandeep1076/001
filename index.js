f37848d61dfc0ca3515de40733272f9419edd026
P. Sandeep Raj <73817589+Sandeep1076@users.noreply.github.com>
2024-07-08 10:18:37 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const navbar = document.getElementById("header");
-    if (window.scrollY > 300) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
